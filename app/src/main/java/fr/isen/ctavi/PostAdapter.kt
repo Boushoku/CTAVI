94623aa63c1df80c7872e7bb598f132b9f5142c1
JetRequin <jetrequin773@gmail.com>
2020-02-04 10:32:53 +0100

    Creation of the wall page

---
+++ app/src/main/java/fr/isen/ctavi/PostAdapter.kt
@@ -0,0 +1,29 @@
+package fr.isen.ctavi
+
+import android.app.Person
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PersonAdapter(val post: ArrayList<PostModel>): RecyclerView.Adapter<PersonAdapter.PersonViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycle_view_wall, parent, false)
+        return PersonViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return post.count()
+    }
+
+    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
+        val person = post[position]
+        holder.bind(person)
+    }
+
+    class PersonViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        fun bind(person: PostModel) {
+
+        }
+    }
+}
---
+++ app/src/main/java/fr/isen/ctavi/PostModel.kt
@@ -0,0 +1,10 @@
+package fr.isen.ctavi
+
+class PostModel {
+    var userName:String? = null
+    var profilePicture:String?= null
+    var postName:String? = null
+    var content:String? = null
+    var ctavie:String? = null
+    var like:String?= null
+}

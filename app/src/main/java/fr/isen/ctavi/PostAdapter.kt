e7f809546b3350187473a2daed9e80cd937a9e0b
Thibaut C <t.chevallier11@gmail.com>
2020-02-06 09:18:40 +0100

    imports

--- app/src/main/java/fr/isen/ctavi/PostAdapter.kt
+++ app/src/main/java/fr/isen/ctavi/PostAdapter.kt
@@ -7,14 +7,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recycle_view_wall.view.*
 
-<<<<<<< HEAD
 
-class PersonAdapter(val post: ArrayList<PostModel>): RecyclerView.Adapter<PersonAdapter.PersonViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
-=======
 class PostAdapter(val post: ArrayList<PostModel>): RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
->>>>>>> 370e1d3626d426b8b1d4f5909bd7c5b0a1489c2e
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recycle_view_wall, parent, false)
         return PostViewHolder(view)
     }
@@ -34,7 +29,7 @@ class PostAdapter(val post: ArrayList<PostModel>): RecyclerView.Adapter<PostAdap
             view.namePost.text=post.postName
             view.post.text=post.content
             view.ctavie.text="CTAVIE "+post.ctavie.toString()
-            view.dislike.text="DISLIKE "+post.dislike.toString()
+            view.like.text="LIKE "+post.dislike.toString()
             Picasso.get().load(post.profilePicture).resize(200, 200).into(view.profilePicture)
 
         }
--- app/src/main/java/fr/isen/ctavi/WallActivity.kt
+++ app/src/main/java/fr/isen/ctavi/WallActivity.kt
@@ -2,6 +2,22 @@ package fr.isen.ctavi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+import java.util.ArrayList
 
 class WallActivity : AppCompatActivity() {
 
@@ -9,6 +25,32 @@ class WallActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wall)
         val postList=ArrayList<PostModel>()
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                val post = dataSnapshot.getValue(PostModel::class.java)
+                // [START_EXCLUDE]
+                post?.let {
+                    postAuthor.text = it.author
+                    postTitle.text = it.title
+                    postBody.text = it.body
+                }
+                // [END_EXCLUDE]
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                // [START_EXCLUDE]
+                Toast.makeText(baseContext, "Failed to load post.",
+                    Toast.LENGTH_SHORT).show()
+                // [END_EXCLUDE]
+            }
+        }
+        postReference.addValueEventListener(postListener)
+
+
         postList.forEach()
         {
             /*val postModel = PostModel()
